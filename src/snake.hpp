14496b66615c20e3344ae6241f022bb1fdb08703
Ben Hollier <b.hollier123@gmail.com>
2015-07-23 19:06:05 +0100

    Update snake.hpp with comments/minor optimisation

--- src/snake.hpp
+++ src/snake.hpp
@@ -7,27 +7,35 @@
 //SFML Headers:
 #include <SFML/Graphics.hpp>
 
+//Enumerator for all four directions:
 enum direction {up, down, right, left, none};
 
 class Snake
 {
     private:
+        //2D Array for displaying board:
         std::array<std::array<int, 40>, 40> board;
+        //Direction the snake is moving:
         direction snakeDirection;
+        //Location of apple:
         sf::Vector2f appleLocation;
 
-        //bool appleOnBoard = false;
+        //So the snake doesn't move slower/faster depending on the framerate:
         sf::Clock snakeMoveClock;
+        //Vector of each pixel of snake body parts:
         std::vector<sf::Vector2f> snakeBodyPartLocations;
 
     public:
+        //Function so the player can change it's direction:
         void changeDirection(direction newSnakeDirection)
         {
+            //If statement so the user doesn't kill themself by moving backwards:
             if (((snakeDirection == up)   && (newSnakeDirection != down))  || ((snakeDirection == down) && (newSnakeDirection != up)) ||
                 ((snakeDirection == left) && (newSnakeDirection != right)) || ((snakeDirection == right) && (newSnakeDirection != left)))
             {
                 snakeDirection = newSnakeDirection;
             }
+            //Unless the snake is only one pixel:
             else if (snakeBodyPartLocations.size() == 1)
             {
                 snakeDirection = newSnakeDirection;
@@ -37,10 +45,13 @@ class Snake
         void update()
         {
             sf::Time elapsedTime = snakeMoveClock.getElapsedTime();
+            //Update every 150ms:
             if (elapsedTime.asMilliseconds() >= 150)
             {
+                //Go through every body part and move:
                 for (int i = signed(snakeBodyPartLocations.size() - 1); i >= 0; --i)
                 {
+                    //If it's the snake's head:
                     if (i == 0)
                     {
                         sf::Vector2f snakeHeadLocation = snakeBodyPartLocations.at(0);
@@ -52,19 +63,33 @@ class Snake
                         snakeBodyPartLocations.at(0) = snakeHeadLocation;
 
                     }
-                    else if (i == signed(snakeBodyPartLocations.size() - 1))
-                    {
-                        snakeBodyPartLocations.at(i) = snakeBodyPartLocations.at(i - 1);
-                    }
+                    //Else:
                     else
                     {
                         snakeBodyPartLocations.at(i) = snakeBodyPartLocations.at(i - 1);
                     }
+                    
+                    //Check if the snake has collided with something:
+                    //If any of the body parts collide with the border:
+                    if ((snakeBodyPartLocations.at(i).x < 1) || (snakeBodyPartLocations.at(i).x > 38) ||
+                        (snakeBodyPartLocations.at(i).y < 1) || (snakeBodyPartLocations.at(i).y > 38))
+                    {
+                        reset(); //Reset the grid
+                    }
+                    
+                    //If any of the body parts collide with the snake's head:
+                    if ((i != 0) && (snakeBodyPartLocations.at(0).x == snakeBodyPartLocations.at(i).x) &&
+                        (snakeBodyPartLocations.at(0).y == snakeBodyPartLocations.at(i).y))
+                    {
+                        reset(); //Reset the grid
+                    }
                 }
 
+                //If the snake's head hit the apple:
                 if ((appleLocation.x == snakeBodyPartLocations.at(0).x) && (appleLocation.y == snakeBodyPartLocations.at(0).y))
                 {
-                    while (true)
+                    //Generate a new apple:
+                    while (true) //While statement untill a apple isn't inside the snake's body
                     {
                         int randomAppleX = rand() % 38 + 1;
                         int randomAppleY = rand() % 38 + 1;
@@ -72,10 +97,13 @@ class Snake
                         {
                             board.at(randomAppleY).at(randomAppleX) = 2;
                             appleLocation = sf::Vector2f(randomAppleX, randomAppleY);
-                            break;
+                            break; //Break because it isn't in the snake's body
                         }
                     }
 
+                    //Grow the Snake:
+                    //If the snake is one pixel, create a new body part 
+                    //in the direction opposite to where it's moving:
                     if (snakeBodyPartLocations.size() == 1)
                     {
                         sf::Vector2f tailLocation = snakeBodyPartLocations.back();
@@ -85,6 +113,8 @@ class Snake
                         else if (snakeDirection == left) {tailLocation.x = tailLocation.x - 1;}
                         snakeBodyPartLocations.push_back(tailLocation);
                     }
+                    //If the snake is larger than one pixel, create a body part 
+                    //that follows the pattern the snake is moving in:
                     else
                     {
                         sf::Vector2f tailLocation = snakeBodyPartLocations.back();
@@ -96,21 +126,7 @@ class Snake
                     }
                 }
 
-                for (int i = 0; i < signed(snakeBodyPartLocations.size()); ++i)
-                {
-                    if ((snakeBodyPartLocations.at(i).x < 1) || (snakeBodyPartLocations.at(i).x > 38) ||
-                        (snakeBodyPartLocations.at(i).y < 1) || (snakeBodyPartLocations.at(i).y > 38))
-                    {
-                        reset();
-                    }
-
-                    if ((i != 0) && (snakeBodyPartLocations.at(0).x == snakeBodyPartLocations.at(i).x) &&
-                        (snakeBodyPartLocations.at(0).y == snakeBodyPartLocations.at(i).y))
-                    {
-                        reset();
-                    }
-                }
-
+                //Update the board:
                 for (int y = 0; y < signed(board.size()); y++) {for (int x = 0; x < signed(board.at(0).size()); ++x)
                 {
                     if ((y == 0) || (y == 39) || (x == 0) || (x == 39)) {board.at(y).at(x) = 3;}
@@ -122,24 +138,30 @@ class Snake
                 }
                 board.at(appleLocation.y).at(appleLocation.x) = 2;
 
+                //Restart the clock
                 snakeMoveClock.restart();
             }
         }
 
         void draw(sf::RenderWindow& targetWindow)
         {
+            //Go through the board:
             for (int y = 0; y < signed(board.size()); y++) {for (int x = 0; x < signed(board.at(0).size()); ++x)
             {
+                //If it's a drawable type:
                 if ((board.at(y).at(x) == 1) || (board.at(y).at(x) == 2) || (board.at(y).at(x) == 3))
                 {
+                    //Create a rectangle:
                     sf::RectangleShape block;
                     block.setSize(sf::Vector2f(10, 10));
                     block.setPosition(x * 10, y * 10);
 
+                    //Colour the rectangle accordingly:
                     if (board.at(y).at(x) == 1) {block.setFillColor(sf::Color::Green);}
                     else if (board.at(y).at(x) == 2) {block.setFillColor(sf::Color::Red);}
                     else if (board.at(y).at(x) == 3) {block.setFillColor(sf::Color::Black);}
 
+                    //Draw the rectangle
                     targetWindow.draw(block);
                 }
             }}
@@ -168,6 +190,7 @@ class Snake
 
         Snake()
         {
+            //Reset the game:
             reset();
         }
 };
