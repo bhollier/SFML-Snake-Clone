b021a6201dbc02dedb61773bbbedec91c10c46ec
Ben Hollier <b.hollier123@gmail.com>
2015-07-23 17:57:22 +0100

    Create game.hpp

---
+++ src/game.hpp
@@ -0,0 +1,36 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+//Standard C++:
+#include <stack>
+//SFML Headers:
+#include <SFML/Graphics.hpp>
+//My Headers:
+#include "ResourceManager.hpp"
+#include "FPS.hpp"
+#include "Snake.hpp"
+
+class Game
+{
+    public:
+        //WINDOW====================================
+        sf::RenderWindow window;
+        bool windowInFocus = true;
+
+        //RESOURCES=================================
+        //Load Game Assets:
+        void loadAssets();
+
+        //GAME OBJECTS==============================
+        FPS gameFPS;
+        Snake snake;
+
+        //GAME LOOP=================================
+        void handleInput();
+        void gameLoop();
+
+        //Constructor:
+        Game();
+};
+
+#endif // GAME_HPP
