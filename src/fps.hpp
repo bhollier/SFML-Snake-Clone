80531e709eef510ef70cd75bcce198e1400b8f81
Ben Hollier <b.hollier123@gmail.com>
2015-07-23 18:44:46 +0100

    Update fps.hpp with comments

--- src/fps.hpp
+++ src/fps.hpp
@@ -12,17 +12,22 @@
 class FPS
 {
     private:
+        //Timer to update the fps:
         sf::Clock fpsClock;
+        //Counter:
         int frameCounter;
+        //Font and Text objects:
         sf::Font fpsDisplayFont;
         sf::Text fpsDisplayText;
 
     public:
+        //Setter functions:
         void setFont(const sf::Font& newFont);
         void setCharacterSize(const int& newSize);
         void setColour(const sf::Color& newColour);
         void setPosition(const sf::Vector2f& newPosition);
 
+        //Draw the text object and count the frames:
         void draw(sf::RenderWindow& targetWindow);
 
         //Constructor:
